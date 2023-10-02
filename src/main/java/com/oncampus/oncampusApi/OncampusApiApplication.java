2fce1869714bce7657bf7c59b401665920d25504
codegeek <usmanxp12@gmail.com>
2023-10-02 20:11:23 +0100

    first commit

---
+++ src/main/java/com/oncampus/oncampusApi/OncampusApiApplication.java
@@ -0,0 +1,13 @@
+package com.oncampus.oncampusApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OncampusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OncampusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oncampus/oncampusApi/OncampusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oncampus.oncampusApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OncampusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
